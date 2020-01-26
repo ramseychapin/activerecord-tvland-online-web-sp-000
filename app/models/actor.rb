cff3578eeb6ecf2757d908a1758da911da8d1236
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-26 20:04:52 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,6 +6,7 @@ class Actor < ActiveRecord::Base
     first_name = self.first_name
     last_name = self.last_name
     result = first_name + " " + last_name
+    "#{self.first_name}"
   end
 
   def list_roles
